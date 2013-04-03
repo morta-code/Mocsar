9ed6d18d55a16fe70db9eaa48f134ea40d78b787
morta <morta@digitus.itk.ppke.hu>
2013-04-03 22:56:00 +0200

    Initial app.js

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 var express = require('express');
+var mongoose = require("mongoose");
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
+
+mongoose.connect("mongodb://localhost/mocsar");
+var db = mongoose.connection;
+
+var mocsarSchema = mongoose.Schema({
+	
+});
+
+var historySchema = mongoose.Schema({
+	
+});
+
 server.listen(7500);
 
