4c8b609086a26c34d13ef9488218205be8c19995
morta <morta@digitus.itk.ppke.hu>
2013-04-23 11:42:20 +0200

    Initial commit for AI

---
+++ ai.js
@@ -0,0 +1,47 @@
+module.exports = function () {
+	return function (db, players) {
+
+		var players = players;
+		var aiPlayers = [];
+
+		
+		function playerAI (player) {
+
+			var onNext = function (nextid) {
+				// TODO
+			};
+
+			var onNextCircle = function (callid) {
+				// TODO
+			};
+
+			var onNewRound = function (neworder) {
+				// TODO
+			};
+
+			var onPut = function (fromId, cards) {
+				// TODO
+			};
+
+			var onTribute = function (tributes) {
+				// TODO
+			};
+
+			var onTributeBack = function () {
+				// TODO
+			};
+
+			return {
+
+			};
+		}
+
+
+
+		return {
+			newAiPlayer: null,
+			aiPlayers: null
+
+		};
+	};
+}();
